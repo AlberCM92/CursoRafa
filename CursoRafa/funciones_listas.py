e05f6507d40a34eb04278ac54d3828c1988602f8
Alberto Carriches <albertocarriches@telefonica.net>
2018-01-26 10:38:47 +0100

    no message

--- CursoRafa/funciones_listas.py
+++ CursoRafa/funciones_listas.py
@@ -2,15 +2,13 @@
 
 import random
 
-print("Juegos:\n", "1. Piedra, papel o tijera.\n", "2. Adivina el numero.");
-
-seleccion_juego = input("¿A que juego quieres jugar? ")
+seleccionar = True
 
 def piedraPapelTijera():
 
 	print("\nBienvenido a piedra, papel o tijera, esto es una partida al mejor de 3.")
 
-	opciones = ["PIEDRA", "PAPEL", "TIJERA"]
+	opcionesPPC = ["PIEDRA", "PAPEL", "TIJERA"]
 
 	puntuacion_jugador = 0
 	puntuacion_maquina = 0
@@ -19,12 +17,12 @@ def piedraPapelTijera():
 		opcion_maquina = random.randint(0,2)
 		opcion_jugador = input("¿Cual será tu arma? (piedra, papel o tijera): ")
 
-		if opcion_jugador.upper() == opciones[0] or opcion_jugador.upper() == opciones[1] or opcion_jugador.upper() == opciones[2] or opcion_jugador.upper() == "TIJERAS":
-			print("\nMaquina: ", opciones[opcion_maquina], " || Jugador: ", opcion_jugador.upper(), "\n")
+		if opcion_jugador.upper() == opcionesPPC[0] or opcion_jugador.upper() == opcionesPPC[1] or opcion_jugador.upper() == opcionesPPC[2] or opcion_jugador.upper() == "TIJERAS":
+			print("\nMaquina: ", opcionesPPC[opcion_maquina], " || Jugador: ", opcion_jugador.upper(), "\n")
 		else:
 			print("\nerror ", opcion_jugador.upper(), " no está en la lista.")
 	
-		if opcion_jugador.upper() == opciones[0]:
+		if opcion_jugador.upper() == opcionesPPC[0]:
 			if opcion_maquina == 0:
 				print("Empate")
 			elif opcion_maquina == 1:
@@ -33,7 +31,7 @@ def piedraPapelTijera():
 			elif opcion_maquina == 2:
 				print("Ganas")
 				puntuacion_jugador += 1
-		elif opcion_jugador.upper() == opciones[1]:
+		elif opcion_jugador.upper() == opcionesPPC[1]:
 			if opcion_maquina == 0:
 				print("Ganas")
 				puntuacion_jugador += 1
@@ -42,7 +40,7 @@ def piedraPapelTijera():
 			elif opcion_maquina == 2:
 				print("Pierdes")
 				puntuacion_maquina += 1
-		elif opcion_jugador.upper() == opciones[2] or opcion_jugador.upper() == "TIJERAS":
+		elif opcion_jugador.upper() == opcionesPPC[2] or opcion_jugador.upper() == "TIJERAS":
 			if opcion_maquina == 0:
 				print("Pierdes")
 				puntuacion_maquina += 1
@@ -60,29 +58,43 @@ def piedraPapelTijera():
 		print("\nHas ganado!!!")
 	print("\nFin de la partida.\n")
 
-
 def adivinaNumero():
 	numero_maquina = random.randint(1, 100)
 	seguir = True
 	contador = 0
 	while contador > 9 or seguir == True:
-		numero_jugador = int(input("Introduce un numero del 1 al 100: "))
-		if numero_jugador == numero_maquina:
-			print("¡Correcto! El numero era: ", numero_jugador, "\nFin de la partida.\n")
-			seguir = False
-		else:
-			if numero_jugador < numero_maquina:
-				print("Mas alto.")
+		numero_jugador = input("Introduce un numero del 1 al 100: ")
+
+		try:
+			if int(numero_jugador) == numero_maquina:
+				print("¡Correcto! El numero era: ", numero_jugador, "\nFin de la partida.")
+				seguir = False
 			else:
-				print("Mas bajo.")
-			print("Intentalo de nuevo.\n")
-			contador += 1
+				if int(numero_jugador) < numero_maquina:
+					print("Mas alto.")
+				else:
+					print("Mas bajo.")
+				print("Intentalo de nuevo.\n")
+				contador += 1
+		except ValueError:
+			print("Eso no es un numero. Vuelve a intentarlo.")
 
 def seleccionaJuego(seleccion_juego):
 	if int(seleccion_juego) == 1:
 		piedraPapelTijera()
 	elif int(seleccion_juego) == 2:
 		adivinaNumero()
+	elif int(seleccion_juego) == 3:
+		print("Hasta pronto!")
+		return
 
-
-seleccionaJuego(seleccion_juego)
+while seleccionar == True:
+	print("Juegos:\n", "1. Piedra, papel o tijera.\n", "2. Adivina el numero.\n", "3. Salir");
+	seleccion_juego = input("¿A que juego quieres jugar? ")
+	if int(seleccion_juego) < 3:
+		seleccionaJuego(seleccion_juego)
+	elif int(seleccion_juego) > 3:
+		print("Ese juego no existe. Selecciona un juego.")
+	elif int(seleccion_juego) == 3:
+		print("Hasta pronto.")
+		seleccionar = False
